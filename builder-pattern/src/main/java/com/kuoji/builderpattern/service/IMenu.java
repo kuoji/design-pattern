f8bf80a90a639f8d33ad14865ad3fa86be48f531
kuoji <978440889@qq.com>
2021-12-03 17:16:50 +0800

    建造者模式

---
+++ builder-pattern/src/main/java/com/kuoji/builderpattern/service/IMenu.java
@@ -0,0 +1,33 @@
+package com.kuoji.builderpattern.service;
+
+
+/**
+ * 装修包接口
+ */
+public interface IMenu {
+
+    /**
+     * 吊顶
+     */
+    IMenu appendCeiling(Matter matter);
+
+    /**
+     * 涂料
+     */
+    IMenu appendCoat(Matter matter);
+
+    /**
+     * 地板
+     */
+    IMenu appendFloor(Matter matter);
+
+    /**
+     * 地砖
+     */
+    IMenu appendTile(Matter matter);
+
+    /**
+     * 明细
+     */
+    String getDetail();
+}
